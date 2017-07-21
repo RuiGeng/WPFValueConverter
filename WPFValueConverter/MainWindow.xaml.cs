d0378180a30e3f8ce60560cabf430f42be7fcbb9
YKF\rui.geng <gengr.r@gmail.com>
2017-07-21 17:19:24 -0400

    Add how to use valueConverter

--- WPFValueConverter/MainWindow.xaml.cs
+++ WPFValueConverter/MainWindow.xaml.cs
@@ -1,17 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace WPFValueConverter
 {
@@ -20,6 +7,35 @@ namespace WPFValueConverter
     /// </summary>
     public partial class MainWindow : Window
     {
+        /// <summary>
+        /// A static property for using ValueConverter
+        /// </summary>
+        public static ValueConverter<string> inputConverter =
+                    new ValueConverter<string>(
+                        inputText1 => inputText1 + inputText1
+                        );
+
+        /// <summary>
+        /// A static property for using MultiValueConverter
+        /// </summary>
+        public static MultiValueConverter<string, string> StringStringConverter =
+            new MultiValueConverter<string, string>(
+                (inputText1, inputText2) =>
+                {
+                    string input12 = string.Empty;
+                    if (!string.IsNullOrEmpty(inputText1))
+                    {
+                        input12 += inputText1;
+                    }
+
+                    if (!string.IsNullOrEmpty(inputText2))
+                    {
+                        input12 += inputText2;
+                    }
+
+                    return string.IsNullOrEmpty(inputText2) ? "No input" : input12 + " MultiValueConverter is cool!";
+                });
+
         public MainWindow()
         {
             InitializeComponent();
