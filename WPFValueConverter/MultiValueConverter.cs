f97f713f628fd966b5a3f2472d0a3f7247debcc6
YKF\rui.geng <gengr.r@gmail.com>
2017-07-21 16:57:26 -0400

    Add Two ValueConverters

---
+++ WPFValueConverter/MultiValueConverter.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace WPFValueConverter
+{
+    public class MultiValueConverter : IMultiValueConverter
+    {
+        private readonly Func<object[], Type, object, CultureInfo, object> convertFunction;
+
+        public MultiValueConverter(Func<object[], Type, object, CultureInfo, object> convertFunction)
+        {
+            this.convertFunction = convertFunction;
+        }
+
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            return convertFunction?.Invoke(values, targetType, parameter, culture);
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+
+    public class MultiValueConverter<T1, T2> : MultiValueConverter
+    {
+        public MultiValueConverter(Func<T1, T2, object> convertFunction)
+            : base(WrapConvertFunction(convertFunction))
+        {
+        }
+
+        private static Func<object[], Type, object, CultureInfo, object> WrapConvertFunction(Func<T1, T2, object> f)
+        {
+            return (objects, targetType, parameter, culture) => f(
+                (T1)System.Convert.ChangeType(objects[0], typeof(T1), culture),
+                (T2)System.Convert.ChangeType(objects[1], typeof(T2), culture)
+                );
+        }
+    }
+
+    public class MultiValueConverter<T1, T2, T3> : MultiValueConverter
+    {
+        public MultiValueConverter(Func<T1, T2, T3, object> convertFunction)
+            : base(WrapConvertFunction(convertFunction))
+        {
+        }
+
+        private static Func<object[], Type, object, CultureInfo, object> WrapConvertFunction(Func<T1, T2, T3, object> f)
+        {
+            return (objects, targetType, parameter, culture) => f(
+                (T1)System.Convert.ChangeType(objects[0], typeof(T1), culture),
+                (T2)System.Convert.ChangeType(objects[1], typeof(T2), culture),
+                (T3)System.Convert.ChangeType(objects[2], typeof(T3), culture)
+                );
+        }
+    }
+
+    public class MultiValueConverter<T1, T2, T3, T4> : MultiValueConverter
+    {
+        public MultiValueConverter(Func<T1, T2, T3, T4, object> convertFunction)
+            : base(WrapConvertFunction(convertFunction))
+        {
+        }
+
+        private static Func<object[], Type, object, CultureInfo, object> WrapConvertFunction(Func<T1, T2, T3, T4, object> f)
+        {
+            return (objects, targetType, parameter, culture) => f(
+                (T1)System.Convert.ChangeType(objects[0], typeof(T1), culture),
+                (T2)System.Convert.ChangeType(objects[1], typeof(T2), culture),
+                (T3)System.Convert.ChangeType(objects[2], typeof(T3), culture),
+                (T4)System.Convert.ChangeType(objects[3], typeof(T4), culture)
+                );
+        }
+    }
+}
---
+++ WPFValueConverter/ValueConverter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace WPFValueConverter
+{
+    public class ValueConverter<T> : ValueConverter
+    {
+        public ValueConverter(Func<T, object> convertFunction)
+            : base(WrapConvertFunction(convertFunction))
+        {
+        }
+
+        private static Func<object, Type, object, CultureInfo, object> WrapConvertFunction(Func<T, object> f)
+        {
+            return (objValue, targetType, parameter, culture) => f(
+                (T)System.Convert.ChangeType(objValue, typeof(T), culture));
+        }
+    }
+
+    public class ValueConverter : IValueConverter
+    {
+        private readonly Func<object, Type, object, CultureInfo, object> convertFunction;
+
+        public ValueConverter(Func<object, Type, object, CultureInfo, object> convertFunction)
+        {
+            this.convertFunction = convertFunction;
+        }
+
+        public object Convert(object value, Type targetType, object parameter = null, CultureInfo culture = null)
+        {
+            return convertFunction?.Invoke(value, targetType, parameter, culture);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
